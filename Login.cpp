bc7071021d236c5ecc08868868c3f58e8584e519
NikolinaMadzharova <nikolina.madjarova@abv.bg>
2025-01-31 16:25:20 +0200

    update Login

--- Login.cpp
+++ Login.cpp
@@ -1,3 +1,17 @@
+/**
+* Solution to course project # 9
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author Nikolina Aleksandrova Madzharova
+* @idnumber 0MI0600464
+* @compiler VC
+*
+* <login information>
+*
+*/
+
 #include "Login.h"
 
 void inputForLogin() {
@@ -15,19 +29,9 @@ void inputForLogin() {
 		}
 		else {
 			cout << "Invalid password"<<endl;
-			cout <<"Enter any character to return to the login/register menu: ";
-		    char x;
-			cin >> x;
-			cout << endl; 
-			printLoginOrRegisterMenu();
 		}
 	} else {
 		cout << "User with this username does not exist"<<endl;
-		cout << "Enter any character to return to the logIn/Register menu:  ";
-		char x;
-		cin >> x;
-		cout << endl;
-		printLoginOrRegisterMenu();
 	}
 }
 
--- Login.h
+++ Login.h
@@ -1,8 +1,19 @@
+/**
+* Solution to course project # 9
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author Nikolina Aleksandrova Madzharova
+* @idnumber 0MI0600464
+* @compiler VC
+*
+* <login information>
+*
+*/
+
 #pragma once
 #include "AccountsData.h"
 #include "Menu.h"
 
 void inputForLogin();
-bool checkIfUserExist(string username);
-bool checkIfThePasswordIsCorrect(string username, string password);
-int getUserIndex(string username);
